98d2d8543c53415b2258888887d2343fa2f89b48
SeyadMohammedFaheem <109874892+SeyadMohammedFaheem@users.noreply.github.com>
2025-10-07 07:50:36 +0000

    tesimanial update

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             <AboutArea />
             <ServiceArea />
             <PortfolioArea />
-             <TestimonoalArea />
+             {/* <TestimonoalArea /> */}
             <BlogArea />
             <ContactArea />
           </main>
